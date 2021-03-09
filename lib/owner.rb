8310139ae5d7b2cbe3abd5fe02a1c9f0b53e7bf2
Owen Mowery <owenmowery99@gmail.com>
2021-03-09 13:43:55 -0500

    commit tree 5ee1b43b66b6fc5795239a19ff1f24e3f1147b57

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,70 @@
+require 'pry'
 class Owner
-  # code goes here
+  
+  attr_reader :name, :species
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end
+
+  def say_species
+    return "I am a #{@species}."
+  end
+
+  def cats
+    Cat.all.select {|cat| cat.owner == self}
+  end
+
+  def dogs
+    Dog.all.select {|dog| dog.owner == self}
+  end
+
+  def buy_cat(name)
+    Cat.new(name, self)
+  end
+
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
+
+  def walk_dogs
+    self.dogs.each {|dog| dog.mood = "happy"}
+  end
+
+  def feed_cats
+    self.cats.each {|cat| cat.mood = "happy"}
+  end
+
+  def sell_pets
+    pets = self.dogs + self.cats
+
+    pets.each do |pet|
+      pet.mood = "nervous"
+      pet.owner = nil
+    end
+  end
+
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end
+
+  #returns all instances of owners
+  def self.all
+    @@all
+  end
+
+  #returns the number of instances of owners
+  def self.count
+    @@all.count
+  end
+
+  #clear the array holding all instances of owners
+  def self.reset_all
+    @@all.clear()
+  end
+
 end
